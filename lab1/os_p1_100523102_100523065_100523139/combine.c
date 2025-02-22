168cca1dd4ed780c6431a6137f2e05ffc4f2a502
rober <robertohogas@gmail.com>
2025-02-22 20:53:59 +0100

    Create an output file and read input files

--- lab1/os_p1_100523102_100523065_100523139/combine.c
+++ lab1/os_p1_100523102_100523065_100523139/combine.c
@@ -5,6 +5,10 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <string.h>
+#include <errno.h>
+
+
+#define PERM 0644 // permissions
 
 struct alumno{
 	char nombre[50];
@@ -21,23 +25,60 @@ int main(int argc, char *argv[]){
         return -1;
     }
 
-	int textFileOne;
-	// Open the first file and check if it was opened correctly
-	textFileOne = open(argv[1], 0);
+	int infile1, infile2, outfile, nread;
 
-	if(textFileOne == -1){
+	// Open the first file and check if it was opened correctly
+
+	// O_RDONLY -> Open the file in binary mode
+	infile1 = open(argv[1], O_RDONLY);
+
+	if(infile1 < 0){
 		perror("The first input file could not be opened");
 		return -1;
 	}
 
-	int textFileTwo;
 	// Open the second file and check if it was opened correctly
-	textFileTwo = open(argv[2], 0);
+	infile2 = open(argv[2], O_RDONLY);
 
-	if(textFileTwo == -1){
+	if(infile2 < 0){
+		close (infile1);
 		perror("The second input file could not be opened");
 		return -1;
 	}
+
+	outfile = creat(argv[3], PERM);
+
+	if (outfile < 0 ) {
+		close (infile1);
+		close (infile2);
+		perror("The output file could not be created!");
+		return 1;
+		}
+
+	int sum = 0;
+	int bufferSize = sizeof(struct alumno);
+	char buffer[bufferSize];
+	struct alumno myAlumn;
+
+	nread = read(infile1, buffer, bufferSize);
+
+	while (nread > 0){
+		nread = read(infile1, buffer, bufferSize);
+	}
+
+	if (nread == -1){
+		printf("An error has ocurred reading the file: %d\n", errno);
+		return -1;
+	}
+
+
+
+	// Close files
+    close(infile1);
+    close(infile2);
+	close(outfile);
+
+
 
 	return 0;
 }
