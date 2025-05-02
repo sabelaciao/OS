79692449b34f12429ca0673a3544fb39fcc58730
rober <robertohogas@gmail.com>
2025-05-02 23:38:01 +0200

    Remove default functions in process_manager.c

--- lab3/process_manager.c
+++ lab3/process_manager.c
@@ -24,17 +24,6 @@ typedef struct {
 } thread_data_t;
 
 
-//Thread function
-void *PrintHello(void *threadid)
-{
-   	long tid;
-   	tid = (long)threadid;
-   	printf("Hello World! It's me, thread #%ld!\n", tid);
-	printf("Thread #%ld ends\n", tid);
-   	pthread_exit(0);
-}
-
-
 void *process_manager(void *arg) {
 	process_data_t *element = (process_data_t *)arg;
 
@@ -173,39 +162,4 @@ void *consumer(void *arg){
 	}
 
 	pthread_exit(NULL);
-}
-
-int process_manager2 (int id, int belt_size, int items_to_produce ){
-	pthread_t threads[NUM_THREADS];
-   	int rc;
-   	long t;
-
-	//using the definded struct/function in queue.c
-	struct element * elemento = NULL;
-	queue_empty();	
-
-	printf("Hello! I am the process manager and I have %d minions.\n", NUM_THREADS);
-
-	for(t=0;t<NUM_THREADS;t++){
-     		printf("Creating thread %ld\n", t);
-		
-		//launching a thread
-     		rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
-     		
-		if (rc){
-       			printf("ERROR; return code from pthread_create() is %d\n", rc);
-       			exit(-1);
-       		}
-     	}
-
-	int i = 0;
-	for(i = 0; i < NUM_THREADS; i++)
-	{	
-		pthread_join(threads[i], NULL);
-	}
-	printf("Fin process manager\n");
-
-   	/* Last thing that main() should do */
-   	return(0);
-	
 }
