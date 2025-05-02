273472670702ace0371fc543fde2425a7151a70c
rober <robertohogas@gmail.com>
2025-05-02 12:17:41 +0200

    Create the variables of the circular queue

--- lab3/queue.c
+++ lab3/queue.c
@@ -3,6 +3,12 @@
 #include <stddef.h>
 #include "queue.h"
 
+static struct element **queue = NULL; // Pointer to the queue
+static int capacity = 0; // Maximum size of the queue
+static int front = 0; // Index of the first element
+static int rear = 0; // Index of the last element
+static int count = 0; // Number of elements in the queue
+
 //To create a queue
 int queue_init(int size){
 	return 0;
